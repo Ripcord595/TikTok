76cea9e42af2dbe40bab5482e3032ca130fcb3a8
Simon <1145564776@qq.com>
2023-08-20 15:26:27 +0800

    将数据库连接的代码重构，可直接使用

--- service/dbconnect.go
+++ service/dbconnect.go
@@ -6,6 +6,11 @@ import (
 )
 
 func DbConnect(writer http.ResponseWriter) (db *sql.DB) {
+	/*
+		连接数据库只需要写：
+			db := service.DbConnect(writer)
+		其中writer是网络响应，在这里用来返回错误状态码
+	*/
 	dsn := "root:123456@tcp(localhost:3306)/tiktok" // 要改成自己的数据源
 	// 连接数据库
 	db, err := sql.Open("mysql", dsn)
