9e2f2881a947f79e96c23837a1bf2f17164435b3
Simon <1145564776@qq.com>
2023-08-22 19:57:30 +0800

    更新用户数据模型

--- model/user.go
+++ model/user.go
@@ -1,15 +1,18 @@
 package model
 
-import "time"
-
 // User 表示用户数据模型
 type User struct {
-	ID        uint      // 用户ID
-	Username  string    // 用户名
-	Email     string    // 电子邮件
-	Password  string    // 密码（可能需要加密存储）
-	CreatedAt time.Time // 创建时间
-	UpdatedAt time.Time // 更新时间
+	Avatar          string `json:"avatar"`           // 用户头像
+	BackgroundImage string `json:"background_image"` // 用户个人页顶部大图
+	FavoriteCount   int64  `json:"favorite_count"`   // 喜欢数
+	FollowCount     int64  `json:"follow_count"`     // 关注总数
+	FollowerCount   int64  `json:"follower_count"`   // 粉丝总数
+	ID              int64  `json:"id"`               // 用户id
+	IsFollow        bool   `json:"is_follow"`        // true-已关注，false-未关注
+	Name            string `json:"name"`             // 用户名称
+	Signature       string `json:"signature"`        // 个人简介
+	TotalFavorited  int    `json:"total_favorited"`  // 获赞数量
+	WorkCount       int64  `json:"work_count"`       // 作品数
 }
 
 // UserRequest 表示用户登录、注册时的请求数据模型
@@ -24,4 +27,10 @@ type UserResponse struct {
 	StatusMsg  string `json:"status_msg,omitempty"`
 	UserID     int64  `json:"user_id,omitempty"`
 	Token      string `json:"token,omitempty"`
+}
+
+type UserInfoGet struct {
+	StatusCode int64   `json:"status_code"` // 状态码，0-成功，其他值-失败
+	StatusMsg  *string `json:"status_msg"`  // 返回状态描述
+	User       *User   `json:"user"`        // 用户信息
 }
